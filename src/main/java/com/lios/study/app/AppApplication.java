3185d410cf2ac6bb466c02e34025fbb54068d48e
李爱光 <aiguang.li@qq.com>
2020-08-09 16:57:23 +0800

    Add druid database pool

--- src/main/java/com/lios/study/app/AppApplication.java
+++ src/main/java/com/lios/study/app/AppApplication.java
@@ -2,13 +2,14 @@ package com.lios.study.app;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author liaiguang
  */
 @MapperScan("com.lios.study.app.mapper")
-@SpringBootApplication
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
